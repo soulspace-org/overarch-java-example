6545cde75678ea5fdaccb9cdb76f991e82a29f09
Ludger Solbach <soulman@soulspace.org>
2024-11-19 09:30:47 +0100

    initial import

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject org.soulspace/overarch-java-example "0.1.0-SNAPSHOT"
+  :dependencies [[org.soulspace/overarch-java-annotations "0.1.0-SNAPSHOT"]
+                 [org.soulspace/overarch-java "0.1.0-SNAPSHOT"]
+                 ;[org.clojure/clojure "1.12.0"]
+                 ]
+  ;:source-paths ["src/main/clojure"]
+  ;:resource-paths ["resources"]
+  :java-source-paths ["src/main/java"]
+  )
---
+++ src/main/java/dummyweb/adapter/DummyRestService.java
@@ -0,0 +1,9 @@
+import org.soulspace.overarch.java.OverarchNode;
+
+/**
+ * This is the REST service for dummies.
+ */
+@OverarchNode(el = "container" /*, id = "my/id" */)
+public class DummyRestService {
+
+}
