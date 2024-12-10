6fce5be576699f17eb53161b09283a273c782d55
Ludger Solbach <soulman@soulspace.org>
2024-12-10 02:20:20 +0100

    added javadoc to email class

--- src/main/java/example/user/domain/EMail.java
+++ src/main/java/example/user/domain/EMail.java
@@ -1,5 +1,10 @@
 package example.user.domain;
 
+import org.soulspace.overarch.java.OverarchNode;
+
+/**
+ * Value object for EMail address
+ */
 @OverarchNode(tags = {"value"})
 public class EMail {
 
